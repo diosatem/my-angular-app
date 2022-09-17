b6b91b4079641c4f4ad33a4825e6499cebe2481a
diosatem <jam19021@byui.edu>
2022-09-18 01:56:05 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'selector-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Diosa's First App";
+  name = "Diosa"
+}
